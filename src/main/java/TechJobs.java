71576c9d10214447a55cd1e8abf1a7df7b3cbfe9
Eric S <estriler@hotmail.com>
2024-09-16 12:00:16 -0500

    printJobs method now prints out jobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,12 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        for(HashMap<String, String> job : someJobs){
+            System.out.println("*****");
+            for(Map.Entry<String, String> item : job.entrySet()){
+                System.out.println(item.getKey() + ": " + item.getValue());
+            }
+            System.out.println("*****\n");
+        }
     }
 }
